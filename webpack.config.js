3fd8ba1ef132c4ce5e27458c82667b24d05590ef
Luiz <doginuwu@gmail.com>
2022-02-04 19:04:00 -0300

    Add webpack serve and sourcemap development

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,35 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: path.resolve(__dirname, 'src', 'index.jsx'),
-    mode: 'development',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
+  mode: "development",
+  devtool: "eval-source-map",
+  entry: path.resolve(__dirname, "src", "index.jsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "public"),
     },
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                use: 'babel-loader'
-            },
-        ],
-    },
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
 };
